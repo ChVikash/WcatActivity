919682016e280f45c9809ef7f5cb37558308bd28
= <vikashch1998@gmail.com>
2021-07-31 13:33:57 +0530

    added reader for multiple paths

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+
 let fs = require("fs");
 
 let inputArr = process.argv.slice(2);
 
+let content = "";
 
-for(let path in inputArr){
+for(let i = 0 ; i < inputArr.length ; ++i){
+    let path = inputArr[i];
     let stats = fs.lstatSync(path);
     if(fs.existsSync(path) == false || stats.isFile() == false){
-        
+        Console.log("File Not Found Error");
+    }
+    else {
+        content += fs.readFileSync(path);
     }
 }
-
+console.log(content);
